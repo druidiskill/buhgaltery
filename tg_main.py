6b750ad70389dacf3419b8ed32c5f2f394386b12
druidiskill <druidiskill@gmail.com>
2024-10-30 21:22:27 +0300

    good_30.10.2024

--- tg_main.py
+++ tg_main.py
@@ -5,6 +5,7 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 import os
 #tg_token = os.environ.get("TOKEN")
+tg_token = "5522797685:AAGoF-k4cyVPt9Kp3adiq3pD7DxQCDJAu4M"
 #
 
 admins = [447392189, 1087504926]
